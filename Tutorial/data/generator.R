f038c660481376db3d8ab5d60c5c5e997fb2500d
MatthewZabka <Matthew.Zabka@smsu.edu>
2018-10-02 16:05:07 -0500

    Updated presentation

--- Tutorial/data/generator.R
+++ Tutorial/data/generator.R
@@ -27,3 +27,21 @@ e2 <- rnorm(n, 0, 0.1)
 S1e <- cbind(cos(t)+ e1, sin(t)+e2)
 plot(S1e)
 write.table(S1e, file = "data3.txt", row.names = FALSE, col.names = FALSE)
+
+# Next let's try S1 V S2.
+n  <- 200
+t  <- runif(n, 0, pi)
+u  <- runif(n, 0, 2*pi)
+v  <- runif(n, 0, 2*pi)
+e1 <- rnorm(n, 0, 0.1)
+e2 <- rnorm(n, 0, 0.1)
+e3 <- rnorm(n, 0, 0.1)
+e4 <- rnorm(n, 0, 0.1)
+e5 <- rnorm(n, 0, 0.1)
+e6 <- rnorm(n, 0, 0.1)
+
+S2e <- cbind(sin(t)*cos(u)-1 +e1, sin(t)*sin(u) + e2, cos(t) + e3)
+S1e <- cbind(cos(v) + 1 + e4, sin(v) + e5, e6)
+S1VS2 <- rbind(S1e, S2e)
+write.table(S1VS2, file = "data4.txt", row.names = FALSE, col.names = FALSE)
+plot(S1VS2[,c(1,3)])
